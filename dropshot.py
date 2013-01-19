6ea4e639e2fdbfee16471118c48d4d020d3ebed6
Luiz Aoqui <lgfa29@gmail.com>
2013-01-19 11:33:01 -0200

    prototype version commit

---
+++ dropshot.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*- 
+
+import os
+import datetime
+import urllib
+
+def warn_no_config_file():
+	os.system('rm .config')
+	raise Exception("Directory {} not found. Please run 'python install.py' again.".format(dropbox_path))
+
+CONFIG_FILE = os.path.join(os.environ['HOME'], 'Scripts/', '.config')
+
+if __name__ == '__main__':
+
+	if not os.path.isfile(CONFIG_FILE):
+		raise Exception("No configuration file found. Run 'python install.py' first.")
+
+	try:
+		user_id, dropbox_path = open(CONFIG_FILE).read().split()
+	except Exception, e:
+		warn_no_config_file()
+
+	if not os.path.isdir(dropbox_path):
+		warn_no_config_file()
+
+	filename = "Screenshot {}.png".format(str(datetime.datetime.now()))
+	file_url = "https://dl.dropbox.com/u/{}/{}".format(user_id, urllib.quote(filename))
+
+	os.system('/usr/sbin/screencapture -tpng -i "{}"'.format(os.path.join(dropbox_path, filename)))
+	os.system('echo "{}" | pbcopy'.format(file_url))
---
+++ install.py
@@ -0,0 +1,45 @@
+# -*- coding: utf8 -*- 
+
+import os
+import sys
+
+SERVICE_PATH = 'DropShot.workflow'
+SCRIPT_PATH = 'dropshot.py'
+CONFIG_PATH = '.config'
+SERVICES_ROOT = os.path.join(os.environ['HOME'], "Library/Services/")
+SCRIPTS_DIR_PATH = os.path.join(os.environ['HOME'], "Scripts/")
+
+
+if __name__ == "__main__":
+	if not os.path.isdir(SERVICE_PATH):
+		raise Exception("DropShot service not found.")
+
+	if not os.path.isfile(SCRIPT_PATH):
+		raise Exception("dropshot.py script not foud")
+
+	if not os.path.isdir(SCRIPTS_DIR_PATH):
+		print "\tCreating Scripts folder..."
+		os.makedirs(SCRIPTS_DIR_PATH)
+
+	if not os.path.isfile(CONFIG_PATH):
+		user_id = raw_input("Enter your DropBox user id number: ")
+		
+		while len(user_id) == 0:
+			print "DropBox user id number can't be blank."
+			user_id = raw_input("Enter your DropBox user id number: ")
+
+		dropbox_public_path = raw_input("Enter your DropBox Public folder path [~/DropBox/Public/]: ")
+
+		if len(dropbox_public_path) == 0:
+			dropbox_public_path = os.path.join(os.environ['HOME'], "DropBox/Public/")
+
+		config_file = open('.config', 'w')
+		config_file.write("{} {}".format(user_id, dropbox_public_path))
+
+
+	print "\tCopying DropShot service..."
+	os.system('cp -r "{}" {}'.format(SERVICE_PATH, SERVICES_ROOT))
+
+	print "\tCopying DropShot script..."
+	os.system('cp {} {}'.format(SCRIPT_PATH, SCRIPTS_DIR_PATH))
+	os.system('cp {} {}'.format(CONFIG_PATH, SCRIPTS_DIR_PATH))
